ddf072e455ff11d66e18ed8cc6d361d7666b7b12
biblicallyTea <71084464+biblicallyTea@users.noreply.github.com>
2025-03-14 05:04:16 +0800

    Update phantom.js
    removed spiritual body on phantom and add fragile
    (spiritual body doesn't let you be attacked)

--- packs/data/gametests/src/data/origins/phantom.js
+++ packs/data/gametests/src/data/origins/phantom.js
@@ -2,10 +2,10 @@
 export const phantom = {
   'powers': [
     'phantomize',
-    'spiritual_body',
     'more_exhaustion',
     'burns_in_daylight',
     'undead',
+    'fragile',
   ],
   'controls': [
     'phantomize'
