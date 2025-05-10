10eab96d03d4b255c055e532d462284eb528e9ba
yardy <jeralddasillo632@gmail.com>
2025-05-10 14:58:05 +0800

    resolves #12 merling breathing issue
    Merling: Fixed being able to breathe air when only their body is underwater.

--- packs/data/gametests/src/data/origins/powers/aqua_affinity.js
+++ packs/data/gametests/src/data/origins/powers/aqua_affinity.js
@@ -1,4 +1,3 @@
-
 import { TicksPerSecond } from "@minecraft/server";
 
 import { toAllPlayers } from "../../../origins/player";
@@ -11,22 +10,23 @@ import { Vector3 } from "../../../utils/Vec3";
 function aqua_affinity(player) {
   /**
    * @param { Vector3 } posOffset 
-   * @returns { import('@minecraft/server').Block }
+   * @returns { import('@minecraft/server').Block | null }
    */
   const getBlock = function(posOffset) {
-    return player.dimension.getBlock(Vector3.add(player.location, posOffset))
-  }
+    return player.dimension.getBlock(Vector3.add(player.location, posOffset));
+  };
+
+  const headBlock = getBlock(new Vector3(0, 1, 0)); // Block at the player's head
 
   if (
     player.hasTag('power_aqua_affinity') &&
-    (getBlock(new Vector3(0, 0, 0)).permutation.matches('water') || getBlock(new Vector3(0, 0, 0)).permutation.matches('flowing_water') ||
-    getBlock(new Vector3(0, 1, 0)).permutation.matches('water') || getBlock(new Vector3(0, 1, 0)).permutation.matches('flowing_water'))
+    headBlock && 
+    (headBlock.permutation.matches('water') || headBlock.permutation.matches('flowing_water'))
   ) {
     player.addEffect('conduit_power', TicksPerSecond * 12, { amplifier: 1, showParticles: false });
   } else {
-    player.removeEffect('conduit_power')
+    player.removeEffect('conduit_power');
   }
-
 }
 
-toAllPlayers(aqua_affinity, 3)
+toAllPlayers(aqua_affinity, 3);
