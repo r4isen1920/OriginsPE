b4181d235e8c2853f70dffe2ab3d1181a8438a1a
yardy <jeralddasillo632@gmail.com>
2025-05-21 22:41:55 +0800

    Merling: adding powers
    added powers : land_breathing_grace, fluid_motion

--- packs/data/gametests/src/data/origins/merling.js
+++ packs/data/gametests/src/data/origins/merling.js
@@ -1,10 +1,12 @@
 
 export const merling = {
   'powers': [
+    'land_breathing_grace',
     'water_breathing',
     'aqua_affinity',
     'sea_creature',
     'like_water',
     'no_trident_damage',
+    'fluid_motion',
   ]
 }
