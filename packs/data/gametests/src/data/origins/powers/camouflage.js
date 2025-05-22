d4e1a73b2c3107fa5e59bf79b6a02ec71528b9d3
r4mstain <rhammzthienne12@gmail.com>
2025-05-22 21:58:30 +0800

    Update camouflage.js

--- packs/data/gametests/src/data/origins/powers/camouflage.js
+++ packs/data/gametests/src/data/origins/powers/camouflage.js
@@ -1,24 +1,36 @@
-
 import { toAllPlayers } from "../../../origins/player";
+
+/**
+ * Calculates the Euclidean distance between two Vector3 objects.
+ * @param { import('@minecraft/server').Vector3 } a 
+ * @param { import('@minecraft/server').Vector3 } b 
+ * @returns { number }
+ */
+function getDistance(a, b) {
+  const dx = a.x - b.x;
+  const dy = a.y - b.y;
+  const dz = a.z - b.z;
+  return Math.sqrt(dx * dx + dy * dy + dz * dz);
+}
 
 /**
  * 
  * @param { import('@minecraft/server').Player } player 
  */
 function camouflage(player) {
-  const mobsNearby = player.dimension.getEntities({
-    location: player.location,
-    maxDistance: 5,
+  const allMobs = player.dimension.getEntities({
     families: ["mob"]
-  })
-  if (player.hasTag('power_camouflage') && player.isSneaking && mobsNearby.length > 0) {
-    player.triggerEvent('r4isen1920_originspe:family_type.camouflage')
+  });
 
+  const isFarFromMobs = allMobs.every(
+    mob => getDistance(mob.location, player.location) > 5
+  );
+
+  if (player.hasTag('power_camouflage') && player.isSneaking && isFarFromMobs) {
+    player.triggerEvent('r4isen1920_originspe:family_type.camouflage');
   } else {
-    
-    player.triggerEvent('r4isen1920_originspe:family_type.player')
-    
+    player.triggerEvent('r4isen1920_originspe:family_type.player');
   }
 }
 
-toAllPlayers(camouflage, 3)
+toAllPlayers(camouflage, 3);
