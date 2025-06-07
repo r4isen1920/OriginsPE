8395675d91ee46de668afcdda5e438bdaf9a4987
r4isen1920 <me@r4isen.net>
2025-06-08 08:11:54 +0900

    Meta: update regolith configs, `@minecraft/server` version, & CI/CD

---
+++ packs/data/gametests/src/index.ts
@@ -0,0 +1,9 @@
+
+import "./origins/controls";
+import "./origins/init";
+import "./origins/item";
+import "./origins/player";
+import "./origins/world";
+
+import "./utils/Math";
+import "./utils/string";
