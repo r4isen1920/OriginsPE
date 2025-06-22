06f6bb2a8659e9149f52a9ab906b4293a8f16133
yardy <jeralddasillo632@gmail.com>
2025-06-22 13:23:24 +0800

    fix 28: Origin selection UI fails to appear mid-air, leaving player permanently invisible and invincible
    must be on ground before you can use the orb of origin

--- packs/data/gametests/src/origins/item.js
+++ packs/data/gametests/src/origins/item.js
@@ -1,4 +1,3 @@
-
 import { EquipmentSlot, ItemStack, TicksPerSecond, system, world } from "@minecraft/server";
 
 import { openOptionsGUI, openScreenPickerGUI, getToggleValue } from "./gui";
@@ -29,6 +28,11 @@ system.runTimeout(() => {
       switch (itemStack.typeId) {
 
         case 'r4isen1920_originspe:orb_of_origins':
+          // Prevent use if not on ground
+          if (!source.isOnGround) {
+            source.onScreenDisplay.setActionBar('You must be on the ground to use the Origin Orb!');
+            break;
+          }
           if (getToggleValue('orb') === 1) {
 
             source.addTag('change_resign');
