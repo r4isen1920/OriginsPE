e5e8a2b93ae9a350267db4b7042a9586894f9387
yardy <jeralddasillo632@gmail.com>
2025-06-22 10:50:31 +0800

    fix #18 and #19
    Choosing the merling origin for the first time will give you 60 seconds to gather resources and go to the water

--- packs/data/gametests/src/data/origins/powers/land_breathing_grace.js
+++ packs/data/gametests/src/data/origins/powers/land_breathing_grace.js
@@ -5,11 +5,13 @@ import { toAllPlayers } from "../../../origins/player";
  * @param { import('@minecraft/server').Player } player 
  */
 function land_breathing_grace(player) {
-  if (!player.hasTag('power_water_breathing')) return;
-  if (player.hasTag('_land_breathing_grace_given')) return; // Already given
+  if (
+    !player.hasTag('power_water_breathing') ||
+    player.hasTag('land_breathing_grace_given')
+  ) return;
 
   player.runCommandAsync('effect @s water_breathing 60 0 true');
-  player.addTag('_land_breathing_grace_given'); // Mark as given
+  player.addTag('land_breathing_grace_given');
 }
 
 toAllPlayers(land_breathing_grace, 3);
