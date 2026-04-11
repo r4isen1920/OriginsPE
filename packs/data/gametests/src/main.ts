741a8ad51f6ae0452e2a530c4e17aa9d61ce731f
r4isen1920 <me@r4isen.net>
2026-04-11 22:27:20 +0800

    Workspace: update config; update CI/CD

---
+++ packs/data/gametests/src/main.ts
@@ -0,0 +1,10 @@
+
+import "./origins/controls.js";
+import "./origins/init.js";
+import "./origins/item.js";
+import "./origins/player.js";
+import "./origins/world.js";
+
+import "./utils/Math.js";
+import "./utils/string.js";
+import "./events/onDeath.js";
