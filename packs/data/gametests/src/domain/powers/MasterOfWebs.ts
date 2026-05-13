82f097f4c9b651961505c50dfd3a2f6a1c8f9dc9
R4M <rhammzthienne12@gmail.com>
2026-05-13 16:19:06 +0800

    Arachnid Power: MasterOfWebs implemented

--- packs/data/gametests/src/domain/powers/MasterOfWebs.ts
+++ packs/data/gametests/src/domain/powers/MasterOfWebs.ts
@@ -1,6 +1,6 @@
 import { Power } from '../Ability';
 import { RegisterPower } from '../Registries';
-
+import { Player } from '@minecraft/server';
 
 /**
  * Granted by default to every player. Acts as a passive marker that other
@@ -11,4 +11,20 @@ import { RegisterPower } from '../Registries';
 @RegisterPower
 export class MasterOfWebs implements Power {
 	readonly id = 'master_of_webs';
+	readonly tickInterval = 5;
+
+	onTick(player: Player): void {
+		const loc = player.location;
+		const block = player.dimension.getBlock(loc);
+
+		if (
+			block?.typeId === 'minecraft:web' ||
+			block?.typeId === 'r4isen1920_originspe:fake_web'
+		) {
+			player.addEffect('resistance', 2, {
+				amplifier: 4,
+				showParticles: false
+			});
+		}
+	}
 }
