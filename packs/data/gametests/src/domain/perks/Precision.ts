d5dfbe5ce18c82e3e1e799c12de4a4a94b2f085d
yardy <jeralddasillo632@gmail.com>
2026-05-19 19:52:44 +0800

    Precision: fix precision

--- packs/data/gametests/src/domain/perks/Precision.ts
+++ packs/data/gametests/src/domain/perks/Precision.ts
@@ -1,10 +1,8 @@
-import { Player, ItemCompleteUseAfterEvent, world, system } from '@minecraft/server';
+import { Player, world, system } from '@minecraft/server';
 
 import { Perk } from '../Ability';
 import { RegisterPerk } from '../Registries';
-import { Entities } from '../../Files';
 import { checkBow } from './Agility';
-
 
 /**
  * When shooting a bow the arrows
@@ -13,30 +11,15 @@ import { checkBow } from './Agility';
 @RegisterPerk
 export class Precision implements Perk {
     readonly id = 'precision';
-    
+
     onTick(player: Player): void {
-        if (!checkBow(player)) return;
+        const isShooting = checkBow(player);
 
-        arrowPrecision(player);
+        if (isShooting) {
+            player.addTag('perk_no_projectile_divergence');
+        } else {
+            player.removeTag('perk_no_projectile_divergence');
+        }
     }
-}
 
-function arrowPrecision(player: Player): void {
-    const arrows = player.dimension.getEntities({
-        type: Entities.Arrow,
-        location: player.location,
-        maxDistance: 2,
-    });
-
-    for (const arrow of arrows) {
-        const velocity = arrow.getVelocity();
-
-        const boosted = {
-            x: velocity.x * 1.5,
-            y: velocity.y * 0.8,
-            z: velocity.z * 1.5,
-        };
-
-        arrow.applyImpulse(boosted);
-    }
 }
