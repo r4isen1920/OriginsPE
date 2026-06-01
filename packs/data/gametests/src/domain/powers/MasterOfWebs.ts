f6c8bede3a60efb584a5e7b65da9aef2fa057350
R4M <rhammzthienne12@gmail.com>
2026-06-01 22:24:58 +0800

    MasterOfWebs: Replace and add the PlayerTick decorator

--- packs/data/gametests/src/domain/powers/MasterOfWebs.ts
+++ packs/data/gametests/src/domain/powers/MasterOfWebs.ts
@@ -1,6 +1,7 @@
 import { Power } from '../Ability';
 import { RegisterPower } from '../Registries';
 import { Player } from '@minecraft/server';
+import { PlayerTick } from '../../core/Ticker';
 
 /**
  * Granted by default to every player. Acts as a passive marker that other
@@ -11,19 +12,27 @@ import { Player } from '@minecraft/server';
 @RegisterPower
 export class MasterOfWebs implements Power {
 	readonly id = 'master_of_webs';
-	readonly tickInterval = 5;
 
-	onTick(player: Player): void {
-		const loc = player.location;
-		const block = player.dimension.getBlock(loc);
-		if (
-			block?.typeId === 'minecraft:web' ||
-			block?.typeId === 'r4isen1920_originspe:fake_web'
-		) {
-			player.addEffect('resistance', 5, {
-				amplifier: 4,
-				showParticles: false
-			});
-		}
+	@PlayerTick(5)
+	static onTick(player: Player): void {
+		try {
+			const loc = player.location;
+			const blockLoc = {
+				x: Math.floor(loc.x),
+				y: Math.floor(loc.y),
+				z: Math.floor(loc.z)
+			};
+
+			const block = player.dimension.getBlock(blockLoc);
+			if (
+				block?.typeId === 'minecraft:web' ||
+				block?.typeId === 'r4isen1920_originspe:fake_web'
+			) {
+				player.addEffect('resistance', 5, {
+					amplifier: 4,
+					showParticles: false
+				});
+			}
+		} catch {}
 	}
 }
