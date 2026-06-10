ba93a04483c6ae1eb6fdb548d96b71fab44f5638
r4isen1920 <me@r4isen.net>
2026-06-10 17:36:20 +0800

    Gills: properly apply `attribute` modification

--- packs/data/gametests/src/domain/powers/Gills.ts
+++ packs/data/gametests/src/domain/powers/Gills.ts
@@ -1,25 +1,11 @@
-import { Player, EntityDamageCause } from '@minecraft/server';
+import { AttributeOverrides } from '../../services/Attributes';
 import { Power } from '../Ability';
-import { PlayerState } from '../../core/PlayerState';
 import { RegisterPower } from '../Registries';
-import { AttributeService } from '../../services/AttributeService';
 
 @RegisterPower
 export class Gills implements Power {
 	readonly id = 'water_breathing';
-	readonly tickInterval = 3;
-
-	onTick(player: Player): void {
-		const state = PlayerState.for(player);
-
-		const isInWater = player.isSwimming || player.isInWater;
-
-		if (isInWater) {
-			AttributeService.apply(player, { breathable: 'underwater' });
-			state.setFlag('gills_active', true);
-		} else {
-			player.applyDamage(1, { cause: EntityDamageCause.drowning });
-			state.setFlag('gills_active', false);
-		}
-	}
+	readonly attributes: AttributeOverrides = {
+		breathable: 'underwater',
+	};
 }
