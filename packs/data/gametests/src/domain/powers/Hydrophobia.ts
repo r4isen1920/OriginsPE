b4d9a16937677bf155ce3ea844be90d24dfdd68c
R4M <rhammzthienne12@gmail.com>
2026-06-19 16:45:37 +0800

    Fix issue #48
    The enderian take damage when in rain

--- packs/data/gametests/src/domain/powers/Hydrophobia.ts
+++ packs/data/gametests/src/domain/powers/Hydrophobia.ts
@@ -1,19 +1,27 @@
-import { Player, EntityDamageCause } from '@minecraft/server';
+import { Player, EntityDamageCause, WeatherType } from '@minecraft/server';
 import { RegisterPower } from '../Registries';
 import { Power } from '../Ability';
+import { AfterWeatherChange } from '../../core/DecoratedEvents';
 
 /**
- * Hydrophobia: this power take damage when swimming or submerged in water.
- * They also play a sizzling sound and emit lava particles while in water.
+ * Hydrophobia: this power takes damage when swimming or submerged in water.
+ * When it rains, Enderians also take damage.
  */
+
+let isRaining = false;
 
 @RegisterPower
 export class Hydrophobia implements Power {
 	readonly id = 'water_vulnerability';
 	readonly tickInterval = 10;
 
+	@AfterWeatherChange()
+	static onWeatherChange(ev: any): void {
+		isRaining = ev.newWeather === WeatherType.Rain || ev.newWeather === WeatherType.Thunder;
+	}
+
 	onTick(player: Player): void {
-		if (player.isSwimming || player.isInWater) {
+		if (player.isSwimming || player.isInWater || isRaining) {
 			player.applyDamage(1, { cause: EntityDamageCause.drowning });
 		}
 	}
