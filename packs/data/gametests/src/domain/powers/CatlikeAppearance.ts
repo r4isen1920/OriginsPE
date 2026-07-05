179f80b01eadd8efbe89b983a1e83c03eea5e53b
r4isen1920 <43404913+r4isen1920@users.noreply.github.com>
2026-07-05 22:41:04 +0800

    CatlikeAppearance: properly be avoided by creepers
    Resolves #64

--- packs/data/gametests/src/domain/powers/CatlikeAppearance.ts
+++ packs/data/gametests/src/domain/powers/CatlikeAppearance.ts
@@ -1,19 +1,13 @@
-import { Player } from '@minecraft/server';
 import { RegisterPower } from '../../core/abilities/Registries';
 import { Power } from '../../core/abilities/Ability';
-import { AttributeService } from '../../services/AttributeService';
-import { PlayerState } from '../../core';
+import { AttributeOverrides } from '../../services';
 
 @RegisterPower
 export class CatlikeAppearance implements Power {
 	readonly id = 'scare_creepers';
 	readonly tickInterval = 3;
 
-	onRelease(player: Player): void {
-        AttributeService.apply(player, { familyType: 'player' });
-    }
-
-    onTick(player: Player): void {
-        AttributeService.apply(player, { familyType: 'cat' });
-    }
+	readonly attributes: AttributeOverrides = {
+		familyType: 'cat'
+	}
 }
