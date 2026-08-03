ab6c0a17b7f625aa2ab71a11e5f6704f6e5b5d7a
r4isen1920 <43404913+r4isen1920@users.noreply.github.com>
2026-08-03 12:15:02 +0800

    NoProjectileDivergence: fix perk ability not triggering properly

--- packs/data/gametests/src/domain/perks/NoProjectileDivergence.ts
+++ packs/data/gametests/src/domain/perks/NoProjectileDivergence.ts
@@ -1,5 +1,5 @@
 
-import { ProjectileHitBlockAfterEvent, ProjectileHitEntityAfterEvent } from '@minecraft/server';
+import { ProjectileHitBlockAfterEvent, ProjectileHitEntityAfterEvent, system } from '@minecraft/server';
 import { AfterProjectileHitBlock, AfterProjectileHitEntity } from '../../core';
 import { Perk } from '../../core/abilities/Ability';
 import { RegisterPerk } from '../../core/abilities/Registries';
@@ -14,15 +14,29 @@ export class NoProjectileDivergence implements Perk {
 	// handled from the `packs/BP/entities/vanilla/arrow.se.templ` already
 
 	@AfterProjectileHitBlock
-	@AfterProjectileHitEntity
-	static onProjectileHit(event: ProjectileHitBlockAfterEvent | ProjectileHitEntityAfterEvent): void {
+	static onProjectileHitBlock(event: ProjectileHitBlockAfterEvent): void {
 		const { projectile, dimension } = event;
 		if (
 			!projectile.isValid ||
-			!projectile.hasTag('_no_projectile_divergence')
+			!projectile.hasTag('r4isen1920_originspe:perk_no_projectile_divergence')
 		) return;
 
 		dimension.spawnParticle(Particles.AccuracyOnHit, projectile.location);
 		dimension.playSound('firework.twinkle', projectile.location, { volume: 0.1, pitch: 1.25 });
+	}
+
+	@AfterProjectileHitEntity
+	static onProjectileHit(event: ProjectileHitEntityAfterEvent): void {
+		const { source, dimension } = event;
+		const victim = event.getEntityHit()?.entity;
+		if (
+			!source || !source.isValid ||
+			!source.hasTag('r4isen1920_originspe:perk_no_projectile_divergence') ||
+			!victim || !victim.isValid
+		) return;
+
+		const aabb = victim.getAABB();
+		dimension.spawnParticle(Particles.AccuracyOnHit, aabb.center);
+		dimension.playSound('firework.twinkle', aabb.center, { volume: 0.1, pitch: 1.25 });
 	}
 }
